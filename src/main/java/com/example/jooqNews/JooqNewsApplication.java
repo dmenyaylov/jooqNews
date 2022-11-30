bc1e114b2e9f91d687f23b6e8c45ff27b260f1a6
dmenyaylov <d.menyaylov@dunice.net>
2022-11-30 14:27:24 +0300

    start

---
+++ src/main/java/com/example/jooqNews/JooqNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooqNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqNewsApplication.class, args);
+	}
+
+}
